b85dccbcdd553543f27c9261f00046761c4114d7
Fangal-Airbag <77993079+Fangal-Airbag@users.noreply.github.com>
2022-01-29 18:48:17 -0500

    Skip patching
    - Added option to skip the Nimble patches by holding ZL on the gamepad when tiramisu is loading
    - Added logging for users to see everything Nimble does
    - Added wut crt patches, very important for modules!

--- source/main.c
+++ source/main.c
@@ -19,10 +19,14 @@
 #include <string.h>
 #include <iosuhax.h>
 
+#include <vpad/input.h>
+#include <whb/log.h>
+#include <whb/log_udp.h>
+
 #include "patches.h"
 
-uint32_t strlen_(const char *str) {
-    const char *s;
+uint32_t strlen_(const char* str) {
+    const char* s;
     for (s = str; *s; ++s);
     return (s - str);
 }
@@ -33,36 +37,58 @@ static void write_string(uint32_t addr, const char* str)
     int len = strlen(str) + 1;
     int remaining = len % 4;
     int num = len - remaining;
-    
+
     for (int i = 0; i < (num / 4); i++) {
-        IOSUHAX_kern_write32(addr + i * 4, *(uint32_t*) (str + i * 4)); 
+        IOSUHAX_kern_write32(addr + i * 4, *(uint32_t*)(str + i * 4));
     }
-    
+
     if (remaining > 0) {
         uint8_t buf[4];
-        IOSUHAX_kern_read32(addr + num, (uint32_t*) &buf, 1);
-        
+        IOSUHAX_kern_read32(addr + num, (uint32_t*)&buf, 1);
+
         for (int i = 0; i < remaining; i++) {
             buf[i] = *(str + num + i);
         }
-        
-        IOSUHAX_kern_write32(addr + num, *(uint32_t*) &buf); 
+
+        IOSUHAX_kern_write32(addr + num, *(uint32_t*)&buf);
     }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    if (IOSUHAX_Open(NULL) >= 0) {
-        /* NoSSL patch */
-        IOSUHAX_kern_write32(0xE1019F78, 0xE3A00001); // mov r0, #1
+    WHBLogUdpInit();
+    
+    WHBLogPrintf("Hello world from Nimble!");
 
-        /* URL patch */
-        for(int i = 0; i < sizeof(url_patches)/sizeof(URL_Patch); i++) {
-            write_string(url_patches[i].address, url_patches[i].url);
-        }
+    VPADStatus status;
+    VPADReadError error = VPAD_READ_SUCCESS;
+    VPADRead(VPAD_CHAN_0, &status, 1, &error);
 
-        IOSUHAX_Close();
+    if (status.hold & VPAD_BUTTON_ZL)
+    {
+        WHBLogPrintf("Nimble patches skipped.");
     }
+    else
+    {
+        if (IOSUHAX_Open(NULL) >= 0) {
+            /* NoSSL patch */
+            IOSUHAX_kern_write32(0xE1019F78, 0xE3A00001); // mov r0, #1
 
+            /* URL patch */
+            for (int i = 0; i < sizeof(url_patches) / sizeof(URL_Patch); i++) {
+                write_string(url_patches[i].address, url_patches[i].url);
+            }
+
+            IOSUHAX_Close();
+            WHBLogPrintf("Nimble patches succeeded!");
+        }
+        else
+        {
+            WHBLogPrintf("Nimble patches failed!");
+        }
+    }
+    
+    WHBLogUdpDeinit();
+    
     return 0;
 }
